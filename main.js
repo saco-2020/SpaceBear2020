a7c3674cef7dcf3edcbd9101d3306d927ee38f0f
UNI-PC2020\Uni <mayarune@yahoo.co.jp>
2020-10-04 04:45:53 +0900

    main.jsにクッキー取得を少し追加

--- main.js
+++ main.js
@@ -10,3 +10,28 @@ function iridium_on(){
 if(value == 1){
     iridium_on()
 }
+
+console.log(getCookie('collection'));
+
+
+function getCookie(key) {
+  var cookieStr = document.cookie;
+
+  // クエリがない場合は空のオブジェクトを返す
+  if (!cookieStr) {
+    return "";
+  }
+
+  // クエリ文字列を & で分割して処理
+  var cookies = cookieStr.split(';');
+  
+  for (var i=0; cookies.length; ++i) {
+    var cookieArr = cookies[i].split('=');
+
+    if (key == cookieArr[0]) {
+      return cookieArr[1];
+    }
+  }
+
+  return "";
+}

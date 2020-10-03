d346ed17b9f1fb808b7d6e852e9673f09c4eba0b
tomoki0307 <goosehouse.sekaowa.melody@gmail.com>
2020-10-04 02:52:44 +0900

    add js to collection.html

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-
+let param =location.search
+let value= param.slice(-1)
+console.log(value)
 function iridium_on(){
-    $("iridium_off").removeClass("iridium_off").addClass("iridium_on")
+    $(".iridium").attr("src","img/Iridium114_satellite.jpg")
+}
+
+if(value == 1){
+    iridium_on()
 }

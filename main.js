f9d6724b47edd630024a5f65bb9a279020d8a38d
tomoki0307 <goosehouse.sekaowa.melody@gmail.com>
2020-10-04 11:59:08 +0900

    Popup作成

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var collectionStr = getCookie('collection');
 var collection = collectionStr.split(',');
 collection = collection.filter(Boolean); // 空データ除去
 
+const Satellite_name=["地球観測衛星 オーラ","国際宇宙ステーション (ISS)"]
+const Satellite_overview=["地球のオゾン層、空気の質、気候を観測する。オーラはテラ（1999年打上げ）とアクア（2002年打上げ）に続く、地球観測システム（EOS）プロジェクト用の3機目の人工衛星になる。2004年7月15日、ヴァンデンバーグ空軍基地からデルタIIロケットによって打ち上げられた。オーラはその他の衛星とA-Trainという衛星コンステレーションを構成する"]
+
 for (var i=0; i<collection.length; ++i) {
   var id = collection[i];
   console.log('ID ' + id + ' の何か処理');
@@ -32,8 +35,14 @@ function getCookie(key) {
 }
 
 
-
 function image_on(id){
-    $(".collection-image:eq("+(id-1)+")").attr("src","img/Satellite"+id+".jpg")
+  $(".collection-image:eq("+(id-1)+")").attr("src","img/Satellite"+id+".jpg").fadein()
+  $(".collection-name:eq("+(id-1)+")").text(Satellite_name[id-1]).fadein()
 }
 
+$('.collection-item').on('click',function(){
+  $('.popup').addClass('show').fadeIn();
+});
+$('.popup-btn').on('click',function(){
+  $('.popup').fadeOut();
+});
